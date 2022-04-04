6ac4c047a1b082a6d2e5aba580b53d6034c5babd
Bruno Almeida <Bruno Almeida@LAPTOP-29OKL3E9>
2022-04-03 22:46:08 -0300

    1

--- BT Rent a Car/src/POO/Main.java
+++ BT Rent a Car/src/POO/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 			
-		Veiculo N = new Veiculo();
+//		Veiculo N = new Veiculo();
 		
-		N.entradaDadosVeiculo();
+//		N.entradaDadosVeiculo();
 		
 	}
 
